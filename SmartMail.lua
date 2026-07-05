2ce98ed0ebdd338d157ec7e5c1b1615d21f1f136
xOrigindx <32461348+xOrigindx@users.noreply.github.com>
2026-07-05 00:39:25 -0700

    test

--- SmartMail.lua
+++ SmartMail.lua
@@ -1,48 +1,10 @@
--- SmartMail.lua (Minimal version)
+DEFAULT_CHAT_FRAME:AddMessage("|cff00ccffSmartMail|r: LUA FILE LOADED!", 1, 1, 1)
 
--- Frame Scripts required by SmartMail.xml
-function SmartMail_OnLoad(frame)
-    frame:RegisterEvent("ADDON_LOADED")
-    table.insert(UISpecialFrames, "SmartMailFrame")
-end
-
-function SmartMail_OnEvent(frame, event)
-    if event == "ADDON_LOADED" and arg1 == "SmartMail" then
-        DEFAULT_CHAT_FRAME:AddMessage("|cff00ccffSmartMail|r: Minimal version loaded! Type /sm", 1, 1, 1)
-    end
-end
-
-function SmartMailLog_OnLoad(frame)
-    table.insert(UISpecialFrames, "SmartMailLogFrame")
-end
-
--- Dummy functions to prevent XML button OnClick errors
-function SmartMail_DeleteProfile() end
-function SmartMail_UpdateProfileList() end
-function SmartMail_SendAll() end
-function SmartMail_SendSelected() end
-function SmartMail_OpenOptions() end
-function SmartMailMinimap_OnLoad() end
-function SmartMailMinimap_OnClick() end
-function SmartMailMinimap_OnEnter() end
-function SmartMailMinimap_OnDragStop() end
-
--- Slash Commands
 SlashCmdList["SMARTMAIL"] = function(msg)
-    if msg == "show" then
-        SmartMailFrame:Show()
-        if SmartMailLogFrame then SmartMailLogFrame:Show() end
-    elseif msg == "hide" then
+    if SmartMailFrame:IsShown() then
         SmartMailFrame:Hide()
-        if SmartMailLogFrame then SmartMailLogFrame:Hide() end
     else
-        if SmartMailFrame:IsShown() then
-            SmartMailFrame:Hide()
-            if SmartMailLogFrame then SmartMailLogFrame:Hide() end
-        else
-            SmartMailFrame:Show()
-            if SmartMailLogFrame then SmartMailLogFrame:Show() end
-        end
+        SmartMailFrame:Show()
     end
 end
 
